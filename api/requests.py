ab6009e1b38c98962c6ba3918083c6f567d8a52d
Raphael Bts <raphael.brethes@outlook.com>
2022-10-29 02:47:29 -0700

    Revert "test"
    This reverts commit 4e0d397f23e8c992cad26bcccbdb043c1570cc34.

--- api/requests.py
+++ api/requests.py
@@ -1,42 +1,73 @@
 from api.scrapper import scrappe
 
-api =  "https://api.myanimelist.net/v2"
-
-def apiSearch(search):
+def apiSearch(search, api="https://www.cinemaspathegaumont.com/api"):
     url = f'{api}/search/quick?q={search}'
     resList = scrappe(url)
     bestMatch = ''
     for res in resList:
-        if res['type'] == 'show' and res['isAnime']:
+        if res['type'] == 'show' and res['isMovie']:
             bestMatch = res['slug']
             break
     return bestMatch
+    
 
-
-def getAnimeInfos(animeId ):
-    url = f'{api}/anime/{animeId}'
+def getMovieInfos(movieName, api="https://www.cinemaspathegaumont.com/api"):
+    url = f'{api}/show/{movieName}'
     res = scrappe(url)
     return res
 
 
-def getAllAnimes():
+def getMovieShowtimes(movieName, theaterName, date="", api="https://www.cinemaspathegaumont.com/api"):
+    url = f'{api}/show/{movieName}/showtimes/{theaterName}'
+    if date != "":
+        url += f'/{date}'
+    res = scrappe(url)
+    return res
+
+
+def getAllShows(api="https://www.cinemaspathegaumont.com/api"):
     url = f'{api}/shows'
     res = scrappe(url)
     return res["shows"]
 
 
+def getAllMovieTheaters(api="https://www.cinemaspathegaumont.com/api"):
+    url = f'{api}/cinemas'
+    res = scrappe(url)
+    return res
 
 
+def getMovieTheaterShows(movieTheater, api='https://www.cinemaspathegaumont.com/api'):
+    url = f'{api}/cinema/{movieTheater}/shows'
+    res = scrappe(url)
+    return res["shows"]
 
+
+def getShowsZone(zone, api="https://www.cinemaspathegaumont.com/api"):
+    url = f'{api}/zone/{zone}'
+    res = scrappe(url)
+    return res["shows"]
 
 # List of all the cities available in gaumont api
+CITY_LIST = [
+    city
+    for city in sorted(list(set(list(map(lambda x: x["citySlug"], getAllMovieTheaters())))))
+]
 
-
-# Dictionary with cities as keys and the list of related Anime theaters as values
-
+# Dictionary with cities as keys and the list of related movie theaters as values
+CINEMA_DICT = {
+    city : sorted([cinema["slug"] for cinema in getAllMovieTheaters() if cinema["citySlug"] == city])
+    for city in CITY_LIST
+}
 
 #region tests
 
+# genres = getMovieInfos('uncharted')['genres']
+# print(genres)
 
+# showtimes = getMovieShowtimes('uncharted', 'cinema-gaumont-alesia')
+# print(showtimes)
+
+# apiSearch('The Batman')
 
 #endregion
--- api/scrapper.py
+++ api/scrapper.py
@@ -1,11 +1,10 @@
 # Scrappe Gaumont API
 import requests
 from requests.exceptions import HTTPError
-header = {'X-MAL-CLIENT-ID':'17710f15775c1c3e6bc63c28ec92c4b2'}
 
 def scrappe(url):
     try:
-        response = requests.get(url,headers=header)
+        response = requests.get(url)
         response.raise_for_status()
         # access JSOn content
         jsonResponse = response.json()
